12130536620edfb74b381eefed68cafd157fb0f2
choref <ouahhidi@gmail.com>
2020-06-12 21:20:41 +0100

    first commit

---
+++ resources/views/layouts/frontend/partial/header.blade.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="container-fluid position-relative no-side-padding">
+
+        <a href="{{ route('home') }}" class="logo">{{ env('APP_NAME') }}</a>
+
+        <div class="menu-nav-icon" data-nav-menu="#main-menu"><i class="ion-navicon"></i></div>
+
+        <ul class="main-menu visible-on-click" id="main-menu">
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('post.index') }}">Posts</a></li>
+            @guest
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+            @else
+                @if(Auth::user()->role->id == 1)
+                    <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                @endif
+                @if(Auth::user()->role->id == 2)
+                    <li><a href="{{ route('author.dashboard') }}">Dashboard</a></li>
+                @endif
+            @endguest
+        </ul><!-- main-menu -->
+
+        <div class="src-area">
+            <form method="GET" action="{{ route('search') }}">
+                <button class="src-btn" type="submit"><i class="ion-ios-search-strong"></i></button>
+                <input class="src-input" value="{{ isset($query) ? $query : '' }}" name="query" type="text" placeholder="Search">
+            </form>
+        </div>
+
+    </div><!-- conatiner -->
+</header>
